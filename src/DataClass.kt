93d3eff722d529a3bc20529faf309104aa80dc88
egambarampanneerselvam <egambaram.panneerselvam@directlinegroup.co.uk>
2019-09-15 16:58:49 +0100

    Ega: Kotlin data class example added with min & max

--- src/DataClass.kt
+++ src/DataClass.kt
@@ -2,10 +2,12 @@ data class Person(val name: String,
                   val age: Int? = null)
 
 fun main(args: Array<String>) {
-    val persons = listOf(Person("Alice",age = 25),
-            Person("Bob", age = 34))
+    val persons = listOf(Person("Ram", age = 32),
+            Person("Ega", age = 29))
 
     val oldest = persons.maxBy { it.age ?: 0 }
+    val younger = persons.minBy { it.age?:0 }
     println("The oldest is: $oldest")
+    println("The younger is: $younger")
 }
 
